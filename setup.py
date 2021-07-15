214d56b77b01cde243772b6b2c66d93072dd0e1e
Patrick Stetz <pstetz@berkeley.edu>
2021-07-15 15:18:08 -0700

    include setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plep",
+    version="0.0",
+    author="PANLab team",
+    author_email="pstetz@stanford.edu",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/WilliamsPANLab/plep",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy>=1.18.1",
+        "pandas>=0.23.4",
+        "tqdm>=4.31.1",
+        "scipy>=1.4.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3", # attention is 3.6.9
+)
