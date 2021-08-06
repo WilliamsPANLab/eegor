8005948946da9fc938807c107679abf28b1884a9
pstetz <pstetz@berkeley.edu>
2021-08-06 09:53:45 -0700

    move load_data and find_file to utils.os

--- eegor/preproc.py
+++ eegor/preproc.py
@@ -1,6 +1,6 @@
 import mne
 from pathlib import Path
-from eegor.os import DuplicateFileError, MissingFileError
+from eegor.utils.os import DuplicateFileError, MissingFileError, load_data, find_file
 
 def preprocess(eeg):
     tmp = eeg.copy().filter(2, 100)
@@ -16,27 +16,14 @@ def ica(epochs):
     ica.fit(epochs)
     ica.plot_sources(epochs, show_scrollbars=True)
 
-def load_data(fp):
-    return mne.io.cnt.read_raw_cnt(fp, preload=False)
-
 def cut(eeg):
     eeg.crop(tmin=10)
-
-def find_file(folder, regex: str):
-    matches = folder.glob(regex)
-    if len(matches) == 0:
-        raise MissingFileError("Cannot find matches for", (folder / regex))
-    elif len(matches) != 1:
-        raise DuplicateFileError("Cannot find matches for", (folder / regex))
-    else:
-        return next(matches)
 
 def main():
     root = Path("/Users/pstetz/Desktop/eeg")
     fp = find_file(root / subject, "*.cnt")
-    eeg = load(fp)
+    eeg = load_data(fp)
     peeg = preprocess(eeg)
-
 
 if __name__ == "__main__":
     main(root)
--- eegor/utils/os.py
+++ eegor/utils/os.py
@@ -1,5 +1,19 @@
+import mne
+
 class DuplicateFileError(Exception):
     pass
 
 class MissingFileError(Exception):
     pass
+
+def load_data(fp):
+    return mne.io.cnt.read_raw_cnt(fp, preload=False)
+
+def find_file(folder, regex: str):
+    matches = folder.glob(regex)
+    if len(matches) == 0:
+        raise MissingFileError("Cannot find matches for", (folder / regex))
+    elif len(matches) != 1:
+        raise DuplicateFileError("Cannot find matches for", (folder / regex))
+    else:
+        return next(matches)
