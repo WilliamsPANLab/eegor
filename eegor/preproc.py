1a93dc4ed4f67fc4632da547726670c2646e41fd
pstetz <pstetz@berkeley.edu>
2021-08-06 09:54:59 -0700

    split eeg data between eyes open and eyes closed

--- eegor/preproc.py
+++ eegor/preproc.py
@@ -16,14 +16,26 @@ def ica(epochs):
     ica.fit(epochs)
     ica.plot_sources(epochs, show_scrollbars=True)
 
-def cut(eeg):
-    eeg.crop(tmin=10)
+def split_eeg(eeg):
+    df = eeg.annotations.to_data_frame()
+    start = df[df["description"] == "1003"]["onset"].iloc[0]
+    half  = (df[df["description"] == "1004"]["onset"].iloc[0] - start).total_seconds()
+    end   = (df[df["description"] == "1002"]["onset"].iloc[0] - start).total_seconds()
+    start = 0
+    eyes_o = eeg.copy().crop(tmin=start, tmax=half)
+    eyes_c = eeg.copy().crop(tmin=half, tmax=end)
+    #FIXME: raise warnings if the length is too short
+    eyes_o.crop(tmin=max(eyes_o.times) - 285, tmax=max(eyes_o.times) - 15)
+    eyes_c.crop(tmin=15, tmax=285)
+    return eyes_o, eyes_c
 
 def main():
     root = Path("/Users/pstetz/Desktop/eeg")
     fp = find_file(root / subject, "*.cnt")
     eeg = load_data(fp)
-    peeg = preprocess(eeg)
+    eo, ec = split_eeg(eeg)
+    pec = preprocess(ec)
+    peo = preprocess(eo)
 
 if __name__ == "__main__":
     main(root)
