3edcff73ba730f1c1e6c298c0bbf3570bd9c8ca9
Patrick Stetz <pstetz@berkeley.edu>
2021-08-16 17:14:55 -0700

    change vis reports interaction

--- eegor/main.py
+++ eegor/main.py
@@ -5,15 +5,20 @@ from eegor.config import config
 import eegor.preprocess.preprocess as preprocess
 from eegor.vis.report import raw_report, autoreject_report
 
-def preproc(config, acq):
-    raw_report(acq, config, subject)
+def preproc(config, acq, subject):
+    root = config["root"]
+    dst = root / "reports" / subject / "raw.html"
+    raw_report(acq, config, dst)
     eo, ec = preprocess.split_eeg(acq, config)
     for trial, raw in [("open", eo), ("closed", ec)]:
+        if trial == "closed": continue
         preprocess.crop_eeg(raw, config, trial=trial)
         processed = preprocess.preprocess(raw, config)
         epochs    = preprocess.epoch(processed, config)
         ar, log, clean = preprocess.reject(epochs, config)
-        autoreject_report(processed, log, epochs, subject, trial, config)
+
+        dst = root / "reports" / subject / f"{trial}_rejected.html"
+        autoreject_report(processed, log, epochs, dst, config)
 
 def main():
     root     = config["root"]
@@ -21,7 +26,7 @@ def main():
     for subject in subjects:
         fp  = find_file(root / subject, "*.cnt")
         acq = load_data(fp)
-        preproc(config, acq)
+        preproc(config, acq, subject)
 
 if __name__ == "__main__":
     main()
--- eegor/vis/report.py
+++ eegor/vis/report.py
@@ -1,14 +1,12 @@
 from eegor.vis.timeseries import plot_eeg_channels, plot_rejects
 from eegor.utils.eeg import get_interval
 
-def raw_report(raw, config, subject):
-    dst = root / "reports" / subject / "raw.html"
+def raw_report(raw, config, dst):
     title = "Raw"
     dst.parent.mkdir(exist_ok=True, parents=True)
     plot_eeg_channels(raw, dst, title, config)
 
-def autoreject_report(acq, ar_log, epochs, subject, trial, config):
-    dst = root / "reports" / subject / f"{trial}_rejected.html"
+def autoreject_report(acq, ar_log, epochs, dst, config):
     title = "Autorejected"
     dst.parent.mkdir(exist_ok=True, parents=True)
     interval = get_interval(epochs)
