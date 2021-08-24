4d3b27ce5dd74a965be7dd8f2a141f92a52c37dd
pstetz <pstetz@berkeley.edu>
2021-08-24 16:18:34 -0700

    separate autoreject to own script

--- eegor/preprocess/preprocess.py
+++ eegor/preprocess/preprocess.py
@@ -1,6 +1,5 @@
 import mne
 import logging
-from autoreject import AutoReject
 
 
 def run_filters(acq, config):
@@ -66,31 +65,3 @@ def crop_eeg(acq, config, trial=None):
     duration = max(acq.times)
     if duration + epsilon - expected_duration < 0:
         print(f"EEG recording of {duration=} is too short")
-
-
-def reject(epochs, config, verbose="tqdm"):
-    """
-    We will denote by κ the maximum number of bad sensors in a non-rejected
-    trial and by ρ the maximum number of sensors that can be interpolated
-    i.e. ρ are n_interpolates and κ are consensus_percs
-
-
-    Please cite Autoreject! The 2nd paper has more info if you can how to
-    choose the n_interpolates and consensus_percs variables
-    [1] Mainak Jas, Denis Engemann, Federico Raimondo, Yousra Bekhti, and
-    Alexandre Gramfort, "Automated rejection and repair of bad trials in
-    MEG/EEG." In 6th International Workshop on Pattern Recognition in
-    Neuroimaging (PRNI), 2016.
-    [2] Mainak Jas, Denis Engemann, Yousra Bekhti, Federico Raimondo, and
-    Alexandre Gramfort. 2017. "Autoreject: Automated artifact rejection for
-    MEG and EEG data". NeuroImage, 159, 417-429.
-    """
-    seed = config["seed"]
-    picks = mne.pick_types(epochs.info, meg=False, eeg=True, stim=False,
-                           eog=True, ecg=False)
-    # Bayesian Optimization is preferred over random_search
-    # https://github.com/autoreject/autoreject/issues/84#issuecomment-341049798
-    ar = AutoReject(thresh_method="bayesian_optimization", random_state=seed,
-                    verbose=verbose, picks=picks)
-    clean, return_log = ar.fit_transform(epochs, return_log=True)
-    return ar, return_log, clean
